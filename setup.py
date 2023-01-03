85e641c8fe92e060ee268bb666c945b5e0c7c2f3
sharathchandra <sharathchandrakarnati6@gmail.com>
2023-01-03 13:44:29 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 #### Declaring variables for setup functions
 Project_name = "Houing_precidtor"
-Version = "0.0.1"
+Version = "0.0.8"
+
 Author = "sharath chandra "
 Description = "This is my first project end to end"
 Packages = ['Housing']
@@ -11,14 +12,14 @@ Requriments_name = "requirements.txt"
 
 def get_requirements()->list[str]:
     """
-    Description : This function is going to return list of 
-    requriments mention in reuqriments .txtfile 
+   # Description : This function is going to return list of 
+    #requriments mention in reuqriments .txtfile 
     
-    return this is function is going to a return a list which contain name of librabries 
-    mentain in requriments .txt file 
+    #return this is function is going to a return a list which contain name of librabries 
+    #mentain in requriments .txt file 
     """
     with open(Requriments_name) as requriment_file:
-        requriment_file.readlines()
+          requriment_file.readlines()#.pop("-e .")
         
     
     
@@ -27,11 +28,10 @@ setup(
     version= Version,
     author= Author,
     description= Description,
-    packages=Packages,
+    packages= find_packages(),#Packages,
     install__requries = get_requirements()
     
 )
 
-if __name__ =="__main__":
-    print(get_requirements())
+
     
